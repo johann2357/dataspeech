58cb8a8b74daccceef8f4a0d4a2dcc04adbc0308
Gabriel Puliatti <gabriel@emptor.io>
2024-05-21 15:21:18 -0500

    Update rate.py

--- dataspeech/cpu_enrichments/rate.py
+++ dataspeech/cpu_enrichments/rate.py
@@ -1,27 +1,27 @@
-from g2p import make_g2p
+from phonemizer import phonemize
+from phonemizer.backend import EspeakBackend
 
-transducer = make_g2p('eng', 'eng-ipa')
+backend = EspeakBackend('es-419', with_stress=True)
 
 def rate_apply(batch, rank=None, audio_column_name="audio", text_column_name="text"):
     if isinstance(batch[audio_column_name], list):  
         speaking_rates = []
         phonemes_list = []
         for text, audio in zip(batch[text_column_name], batch[audio_column_name]):
-            phonemes = transducer(text).output_string
+            phonemes = phonemize(text, language='es-es', backend='espeak', with_stress=True)
             
             sample_rate = audio["sampling_rate"]
             audio_length = len(audio["array"].squeeze()) / sample_rate
             
             speaking_rate = len(phonemes) / audio_length
 
-            
             speaking_rates.append(speaking_rate)
             phonemes_list.append(phonemes)
         
         batch["speaking_rate"] = speaking_rates
         batch["phonemes"] = phonemes_list
     else:
-        phonemes = transducer(batch[text_column_name]).output_string
+        phonemes = phonemize(batch[text_column_name], language='es-es', backend='espeak', with_stress=True)
             
         sample_rate = batch[audio_column_name]["sampling_rate"]
         audio_length = len(batch[audio_column_name]["array"].squeeze()) / sample_rate
