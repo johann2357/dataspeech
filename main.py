3f86ab394ea909758451622a2ff700f5e79c0d5f
Johann <johann@emptor.io>
2024-12-20 16:48:24 -0500

    feat: process partial parquet files

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import fnmatch
 import json
 
 from datasets import load_dataset
@@ -55,7 +57,8 @@ if __name__ == "__main__":
     set_start_method("spawn")
     parser = argparse.ArgumentParser()
     parser.add_argument("dataset_name", type=str, help="Path or name of the dataset. See: https://huggingface.co/docs/datasets/v2.17.0/en/package_reference/loading_methods#datasets.load_dataset.path")
-    parser.add_argument("--data_files", default=None, type=str, help="File or list of files to process")
+    parser.add_argument("--data_file", default=None, type=str, help="File to process")
+    parser.add_argument("--data_file_pattern", default=None, type=str, help="File to process")
     parser.add_argument("--configuration", default=None, type=str, help="Dataset configuration to use, if necessary.")
     parser.add_argument("--output_dir", default=None, type=str, help="If specified, save the dataset on disk with this path.")
     parser.add_argument("--repo_id", default=None, type=str, help="If specified, push the dataset to the hub.")
@@ -71,68 +74,76 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if args.data_files:
-        dataset = load_dataset(
-            args.dataset_name,
-            data_files=args.data_files,
-            num_proc=args.cpu_num_workers,
-        )
-    elif args.configuration:
-        dataset = load_dataset(args.dataset_name, args.configuration, num_proc=args.cpu_num_workers)
+    if args.data_file_pattern:
+        files = [f for f in os.listdir(args.dataset_name) if fnmatch.fnmatch(f, args.data_file_pattern)]
+    elif args.data_file:
+        files = [args.data_file]
     else:
-        dataset = load_dataset(args.dataset_name, num_proc=args.cpu_num_workers)
+        files = [None]
 
-    audio_column_name = "audio" if args.rename_column else args.audio_column_name
-    text_column_name = "text" if args.rename_column else args.text_column_name
-    if args.rename_column:
-        dataset = dataset.rename_columns({args.audio_column_name: "audio", args.text_column_name: "text"})
+    for file in files:
+        if os.path.exists(f"output/{file}"):
+            print(f"File {file} already processed")
+            continue
 
-    print("Compute pitch")
-    pitch_dataset = map_with_dynamic_batch_size(
-        dataset=dataset,
-        function=pitch_apply,
-        initial_batch_size=args.batch_size,
-        min_batch_size=1,
-        num_proc=torch.cuda.device_count()*args.num_workers_per_gpu_for_pitch if torch.cuda.device_count()>0 else args.cpu_num_workers,
-        with_rank=True if torch.cuda.device_count()>0 else False,
-        remove_columns=[audio_column_name],  # tricks to avoid rewritting audio
-        fn_kwargs={"audio_column_name": audio_column_name, "penn_batch_size": args.penn_batch_size},
-    )
-
-    print("Compute snr and reverb")
-    snr_dataset = map_with_dynamic_batch_size(
-        dataset=dataset,
-        function=snr_apply,
-        initial_batch_size=args.batch_size,
-        min_batch_size=1,
-        num_proc=torch.cuda.device_count()*args.num_workers_per_gpu_for_snr if torch.cuda.device_count()>0 else args.cpu_num_workers,
-        with_rank=True if torch.cuda.device_count()>0 else False,
-        remove_columns=[audio_column_name],  # tricks to avoid rewritting audio
-        fn_kwargs={"audio_column_name": audio_column_name},
-    )
-
-    print("Compute speaking rate")
-    rate_dataset = dataset.map(
-        rate_apply,
-        with_rank=False,
-        num_proc=args.cpu_num_workers,
-        writer_batch_size=args.cpu_writer_batch_size,
-        remove_columns=[audio_column_name],  # tricks to avoid rewritting audio
-        fn_kwargs={"audio_column_name": audio_column_name, "text_column_name": text_column_name},
-    )
-
-    print("Rate dataset", rate_dataset)
-    for split in dataset.keys():
-        if dataset[split]:
-            dataset[split] = pitch_dataset[split].add_column("snr", snr_dataset[split]["snr"]).add_column("c50", snr_dataset[split]["c50"])
-            dataset[split] = dataset[split].add_column("speaking_rate", rate_dataset[split]["speaking_rate"]).add_column("phonemes", rate_dataset[split]["phonemes"])
-
-    if args.output_dir:
-        print("Saving to disk...", dataset)
-        dataset.save_to_disk(args.output_dir)
-    if args.repo_id:
-        print("Pushing to the hub...", dataset)
-        if args.configuration:
-            dataset.push_to_hub(args.repo_id, args.configuration)
+        if file:
+            dataset = load_dataset(
+                args.dataset_name,
+                data_files=file,
+                num_proc=args.cpu_num_workers,
+            )
+        elif args.configuration:
+            dataset = load_dataset(args.dataset_name, args.configuration, num_proc=args.cpu_num_workers)
         else:
-            dataset.push_to_hub(args.repo_id)
+            dataset = load_dataset(args.dataset_name, num_proc=args.cpu_num_workers)
+
+        audio_column_name = "audio" if args.rename_column else args.audio_column_name
+        text_column_name = "text" if args.rename_column else args.text_column_name
+        if args.rename_column:
+            dataset = dataset.rename_columns({args.audio_column_name: "audio", args.text_column_name: "text"})
+
+        print("Compute pitch")
+        pitch_dataset = map_with_dynamic_batch_size(
+            dataset=dataset,
+            function=pitch_apply,
+            initial_batch_size=args.batch_size,
+            min_batch_size=1,
+            num_proc=torch.cuda.device_count()*args.num_workers_per_gpu_for_pitch if torch.cuda.device_count()>0 else args.cpu_num_workers,
+            with_rank=True if torch.cuda.device_count()>0 else False,
+            remove_columns=[audio_column_name],  # tricks to avoid rewritting audio
+            fn_kwargs={"audio_column_name": audio_column_name, "penn_batch_size": args.penn_batch_size},
+        )
+
+        print("Compute snr and reverb")
+        snr_dataset = map_with_dynamic_batch_size(
+            dataset=dataset,
+            function=snr_apply,
+            initial_batch_size=args.batch_size,
+            min_batch_size=1,
+            num_proc=torch.cuda.device_count()*args.num_workers_per_gpu_for_snr if torch.cuda.device_count()>0 else args.cpu_num_workers,
+            with_rank=True if torch.cuda.device_count()>0 else False,
+            remove_columns=[audio_column_name],  # tricks to avoid rewritting audio
+            fn_kwargs={"audio_column_name": audio_column_name},
+        )
+
+        print("Compute speaking rate")
+        rate_dataset = dataset.map(
+            rate_apply,
+            with_rank=False,
+            num_proc=args.cpu_num_workers,
+            writer_batch_size=args.cpu_writer_batch_size,
+            remove_columns=[audio_column_name],  # tricks to avoid rewritting audio
+            fn_kwargs={"audio_column_name": audio_column_name, "text_column_name": text_column_name},
+        )
+
+        print("Rate dataset", rate_dataset)
+        for split in dataset.keys():
+            if dataset[split]:
+                dataset[split] = pitch_dataset[split].add_column("snr", snr_dataset[split]["snr"]).add_column("c50", snr_dataset[split]["c50"])
+                dataset[split] = dataset[split].add_column("speaking_rate", rate_dataset[split]["speaking_rate"]).add_column("phonemes", rate_dataset[split]["phonemes"])
+                text_description = [
+                    "A male quechua speaker from central Peru delivers his speech in a somewhat quiet and confined space, with a mildly monotone tone, a slightly low pitch, and a slow pace. The recording appears to be of good quality with only a slight background noise.",
+                ] * len(dataset[split]["speaker_id"])
+                dataset[split] = dataset[split].add_column("text_description", text_description)
+                df = dataset[split].to_pandas()
+                df.to_parquet(f"output/{file}", engine='pyarrow')
